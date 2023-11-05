8e3c429c6dac525bfafdb9a5cbaa81ef5fbdc0a0
nhasan97 <nh6069325@gmail.com>
2023-11-05 11:13:10 +0600

    added the navbar

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../reusableComponents/Footer";
+import Navbar from "../reusableComponents/Navbar/Navbar";
 
 const Root = () => {
   return (
     <div>
+      <div className="flex justify-center relative">
+        <Navbar></Navbar>
+      </div>
       <div className="max-w-screen-xl mx-auto px-20">
         <Outlet></Outlet>
       </div>
-
       <Footer></Footer>
     </div>
   );
